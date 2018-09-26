d5f0b49a0e0fb475f4e905c98ceb9dc733e17bca
mazx <dyxdy@live.com>
2018-09-26 05:57:55 -0400

    multi server

--- src/socktest.c
+++ src/socktest.c
@@ -16,8 +16,9 @@
 #include "adlist.h"
 #include "zmalloc.h"
 
-#define UNUSED(V) ((void) V)
-#define RANDPTR_INITIAL_SIZE 8
+const int OPSIZE = 2000001;
+const int BUFSIZE = 4500;
+const int SERVERSIZE = 16;
 
 static long long ustime(void) {
     struct timeval tv;
@@ -39,46 +40,93 @@ static long long mstime(void) {
     return mst;
 }
 
+unsigned short crc16(const char *buf, int len);
+
+int getServer(char *buf, int len, int nserver) {
+	int t = crc16(buf, len) & 0x3FFF;
+	int p = t * nserver / 16384;
+	if (p < 0 || p > 2) printf("!!!!! %d\n", p);
+	return p;
+}
+
 int main(int argc, const char **argv) {
-	redisContext *context;
+	redisContext* c[SERVERSIZE];
 
-	char buf[256] = "*3\r\n$3\r\nSET\r\n$1\r\nA\r\n$1\r\nB\r\n";
-	char reply[256];
-	char hostip[64] = "127.0.0.1";
+	char **optab;
+	optab = malloc(OPSIZE * sizeof(char *));
+	int *ops;
+	ops = malloc(OPSIZE * sizeof(int));
+	long long *oplat;
+	oplat = malloc(OPSIZE * sizeof(long long));
+	long long *optime;
+	optime = malloc(OPSIZE * sizeof(long long));
+	int opid = 0, opnum = 0;
+
+	FILE *pfc = fopen("config.txt", "r");
+	char hostip[64];
 	int hostport;
-	hostport = 6379;
+	int nserver;
 
-	context = redisConnectNonBlock(hostip, hostport);
-	int len;
-	len = send(context->fd, buf, strlen(buf), 0);
-	printf("%d\n", len);
-	
-	while (1) {
-		len = recv(context->fd, reply, 256, 0);
-		if (len > 0) break;
+	fscanf(pfc, "%d", &nserver);
+	for (int i = 0; i < nserver; i++) {
+		fscanf(pfc, "%s%d", hostip, &hostport);
+		c[i] = redisConnectNonBlock(hostip, hostport);
 	}
-	printf("%s\n", reply);
-	/*
-	FILE *pf = fopen("/home/mazx/work/bigt.in", "r");
-	char buf[100], tmp[100];
-	fgets(buf, 100, pf);
-	sscanf(buf, "%d", &opnum);
-	//if (config.requests < opnum) opnum = config.requests;
-	opid = 0;
-	for (int i = 0; i < opnum; i++) {
-		char ch;
-		int t;
-		fgets(buf, 99, pf);
+	char path[128];
+	fscanf(pfc, "%s", path);
+	printf("%s\n", path);
+	fclose(pfc);
+	long long op, ed;
+	long long tin, tout;
+
+	int len;
+	
+	FILE *pf = fopen(path, "r");
+	char buf[BUFSIZE], tmp[BUFSIZE];
+	int t;
+
+	int cnt[3];
+	memset(cnt, 0, sizeof(cnt));
+	while (fgets(buf, BUFSIZE, pf)) {
 		sscanf(buf+1, "%d", &t);
 		strcpy(tmp, buf);
-		for (int j = 0; j < 2 * t; j++) {
-			fgets(buf, 99, pf);
+		for (int i = 0; i < 2 * t; i++) {
+			fgets(buf, BUFSIZE, pf);
 			strcat(tmp, buf);
-
+			if (i == 3) {
+				ops[opnum] = getServer(buf, strlen(buf) - 2, nserver);
+			}
 		}
-		strcpy(optable[i], tmp);
+		fgets(buf, BUFSIZE, pf);
+		optab[opnum] = malloc(strlen(tmp) + 1);
+		strcpy(optab[opnum], tmp);
+		cnt[ops[opnum]]++;
+		opnum++;
 	}
-	*/
-
+	for (int i = 0; i < 3; i++) printf("%d ", cnt[i]); printf("\n");
+	printf("load finished\n");
+	
+	char rep[BUFSIZE];
+	tin = mstime();
+	for (int i = 0; i < opnum; i++) {
+		op = ustime();
+		len = send(c[ops[i]]->fd, optab[i], strlen(optab[i]), 0);
+		while (1) {
+			len = recv(c[ops[i]]->fd, rep, 256, 0);
+			if (len > 0) break;
+		}
+		ed = ustime();
+		oplat[opid] = ed - op;
+		optime[opid] = mstime();
+		opid++;
+		if (!(i % 10000)) printf("%d finished!\n", i);
+	}
+	tout = mstime();
+	int mn = 1e7, mx = 0;
+	for (int i = 0; i < opnum; i++) {
+		if (oplat[i] > mx) mx = oplat[i];
+		if (oplat[i] < mn) mn = oplat[i];
+	}
+	printf("%d %d %lld\n", mx, mn, tout-tin);
 	return 0;
 }
